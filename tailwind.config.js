136ce04ad817a0a708ef2169ecf6c15aaf11dc7e
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-12-19 22:43:23 +0600

    tailwind css framework website cloning file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
